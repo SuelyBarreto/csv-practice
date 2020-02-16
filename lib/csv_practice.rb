1df90a6622224b5a7ee5efca5858e9e5e79ccdd6
SuelyBarreto <suely@barreto.us>
2020-02-15 20:01:07 -0800

    practice

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -4,22 +4,22 @@ require 'csv'
 require 'awesome_print'
 
 # ID,Name,Sex,Age,Height,Weight,Team,NOC,Games,Year,Season,City,Sport,Event,Medal
-# 0  1    2    3   4      5      6    7    8    9    10     11   12    13    14
+# 0  1    2    3   4      5      6    7    8    9    10     11   12    13    14 
 
 # ID Name Height Team Year City Sport Event Medal
 
 def get_all_olympic_athletes(filename)
 
   all_athletes = CSV.read(filename, headers: true).map do |athlete|
-    {"ID" => athlete[0],
-    "Name" => athlete[1],
+    {"ID"    => athlete[0],
+    "Name"   => athlete[1],
     "Height" => athlete[4],
-    "Team" => athlete[6],
-    "Year" => athlete[9],
-    "City" => athlete[11],
-    "Sport" => athlete[12],
-    "Event" => athlete[13],
-    "Medal" => athlete[14]}
+    "Team"   => athlete[6],
+    "Year"   => athlete[9],
+    "City"   => athlete[11],
+    "Sport"  => athlete[12],
+    "Event"  => athlete[13],
+    "Medal"  => athlete[14]}
   end
 
   return all_athletes
@@ -34,7 +34,7 @@ def total_medals_per_team(olympic_data)
 
     # team = athlete["Team"]
     # medal = athlete["Medal"]
-    # olympic_teams[team] += medal == "NA" ? 0 : 1
+    # olympic_teams[team] += me dal == "NA" ? 0 : 1
     # if medal != "NA"
     #   olympic_teams[team] += 1
     # end
